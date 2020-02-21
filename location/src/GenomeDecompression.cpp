900ea271a7a1465d913bbbdf52965e8e723a94aa
Lucy Hancock <lucy@lucyhancock.tech>
2020-02-21 20:15:07 +0000

    Decompression implementation

--- location/src/GenomeDecompression.cpp
+++ location/src/GenomeDecompression.cpp
@@ -7,6 +7,27 @@ namespace crisprsearch::location {
     }
 
     unique_ptr<char[]> GenomeDecompression::decompress() {
-        return nullptr;
+        // Decompress file using boost gzip filter
+        ifstream toDecompress(genomePath, ios_base::in | ios_base::binary);
+
+        // Check file exists
+        if(!toDecompress.good()) {
+            throw FileNotGoodException();
+        }
+
+        // Do decompression
+        filtering_streambuf<input> decompressionStream;
+        decompressionStream.push(gzip_decompressor());
+        decompressionStream.push(toDecompress);
+        stringstream output;
+        boost::iostreams::copy(decompressionStream, output);
+
+        toDecompress.close();
+
+        // Copy into output pointer
+        unique_ptr<char[]> outputPtr = make_unique<char[]>(output.str().length() + 1);
+        memcpy(outputPtr.get(), output.str().c_str(), output.str().length());
+        outputPtr.get()[output.str().length()] = '\0';
+        return outputPtr;
     }
 }
