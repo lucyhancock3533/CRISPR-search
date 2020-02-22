aaad94335e0e1cb880ce593ab0440759843a12ef
Lucy Hancock <lucy@lucyhancock.tech>
2020-02-22 23:20:14 +0000

    Created Record class

---
+++ location/src/Record.hpp
@@ -0,0 +1,12 @@
+#ifndef CRISPR_SEARCH_LOCATION_RECORD_HPP
+#define CRISPR_SEARCH_LOCATION_RECORD_HPP
+
+#include "cslocation.hpp"
+
+namespace crisprsearch::location {
+    class Record {
+
+    };
+}
+
+#endif
