fb62e26e1fb33d8dbc45e6ecc7bbd76eed8ed4bd
Lucy Hancock <lucy@lucyhancock.tech>
2020-02-24 00:33:44 +0000

    Docs for record class

--- location/src/Record.hpp
+++ location/src/Record.hpp
@@ -4,6 +4,9 @@
 #include "cslocation.hpp"
 
 namespace crisprsearch::location {
+    /**
+     * Spacer record storage class
+     */
     class Record {
     private:
         string id;
@@ -12,11 +15,38 @@ namespace crisprsearch::location {
         int startPos;
         int endPos;
     public:
+        /**
+         * Create a new spacer record from values
+         * @param sequence Genome sequence
+         * @param type Type (leftFlank, rightFlank, spacer, DR)
+         * @param startPos Start position within scaffold
+         * @param endPos End position within scaffold
+         */
         explicit Record(string sequence, string type, int startPos, int endPos);
+        /**
+         * Get the auto-generated ID for this record
+         * @return UUID of record
+         */
         string getId();
+        /**
+         * Get the genome sequence of this record
+         * @return String of sequence
+         */
         string getSequence();
+        /**
+         * Get the type of this record
+         * @return String of record type (leftFlank, rightFlank, spacer, DR)
+         */
         string getType();
+        /**
+         * Get the start position of this record within the scaffold
+         * @return Start position within scaffold
+         */
         int getStartPos();
+        /**
+         * Get the end position of this record within the scaffold
+         * @return End position within scaffold
+         */
         int getEndPos();
     };
 }
