048e9a6a45c31d9fc82ce5e54b807845015d6c28
Lucy Hancock <lucy@lucyhancock.tech>
2020-02-27 20:38:04 +0000

    SQL for genome insert

--- location/src/csl_constants.hpp
+++ location/src/csl_constants.hpp
@@ -6,6 +6,7 @@
 #define SQLITE_CREATE_CRISPR_TABLE "CREATE TABLE CRISPR(id UUID PRIMARY KEY NOT NULL, genomeId UUID NOT NULL, sourceSequence TEXT NOT NULL, drLength NUMBER NOT NULL, spacers NUMBER NOT NULL, evidenceLevel NUMBER NOT NULL, regions NUMBER NOT NULL);"
 #define SQLITE_CREATE_REGION_TABLE "CREATE TABLE Regions(id UUID PRIMARY KEY NOT NULL, crisprId UUID NOT NULL, sequence TEXT NOT NULL, type TEXT NOT NULL, regionNo NUMBER NOT NULL, startPos NUMBER NOT NULL, endPos NUMBER NOT NULL);"
 #define SQLITE_INSERT_REGION "INSERT INTO Regions (id, crisprId, sequence, type, regionNo, startPos, endPos) VALUES (?, ?, ?, ?, ?, ?, ?);"
-#define SQLITE_INSERT_CRISPR "INSERT INTO CRISPR (id, genomeId, sourceSequence, drLength, spacers, evidenceLevel, regions) VALUES (?, ?, ?, ?, ?, ?, ?)"
+#define SQLITE_INSERT_CRISPR "INSERT INTO CRISPR (id, genomeId, sourceSequence, drLength, spacers, evidenceLevel, regions) VALUES (?, ?, ?, ?, ?, ?, ?);"
+#define SQLITE_INSERT_GENOME "INSERT INTO Genomes (id, name, genomeInfo, genomeSource) VALUES (?, ?, ?, ?);"
 
 #endif
