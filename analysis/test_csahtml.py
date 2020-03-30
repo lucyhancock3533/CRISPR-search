683418079297b037391743724ece4612526fe2ea
Lucy Hancock <lucy@lucyhancock.tech>
2020-03-30 19:57:01 +0100

    Skeleton html generator classes

---
+++ analysis/test_csahtml.py
@@ -0,0 +1,18 @@
+import unittest
+from csahtmlgen import CsaHTMLGenerator
+
+class TestCsaHTMLGeneration(unittest.TestCase):
+    def testValidStatCalc(self):
+        pass
+
+    def testInvalidStatCalc(self):
+        pass
+
+    def testValidDistCalc(self):
+        pass
+
+    def testInvalidDistCalc(self):
+        pass
+
+    def testHTMLGen(self):
+        pass
