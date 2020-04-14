5444c85c77d1a17d0711f83a37f59d73792d7e57
Lucy Hancock <lucy@lucyhancock.tech>
2020-04-14 20:51:37 +0100

    Fasta DB generator tests

--- analysis/test_csablast.py
+++ analysis/test_csablast.py
@@ -1,4 +1,24 @@
 import unittest
+import uuid
+from csadb import DbConnection
+from csablast import FastaDbGen
 
 class TestFastaGen(unittest.TestCase):
-    pass
+    db = DbConnection()
+
+    def testFastaDbGen(self):
+        self.db.changeConnDb('test_dbs/' + str(uuid.uuid4()) + '.db')
+        self.db.loadDatabase('test_dbs/rug900.sdb')
+        f = FastaDbGen(self.db.getCursor(), 3)
+        f.generateFastaDb()
+        self.assertIsNotNone(f.fastaDb)
+        # Assert expected database
+
+    def testFastaGen(self):
+        self.db.changeConnDb('test_dbs/' + str(uuid.uuid4()) + '.db')
+        self.db.loadDatabase('test_dbs/rug900.sdb')
+        f = FastaDbGen(self.db.getCursor(), 3)
+        f.generateFasta()
+        self.assertIsNotNone(f.fastaDb)
+        self.assertIsNotNone(f.fasta)
+        self.assertNotEquals(0, len(f.fasta))
