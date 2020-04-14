7b7be65c35986e0be2993edbe37560d3f6a45e1e
Lucy Hancock <lucy@lucyhancock.tech>
2020-04-14 21:14:10 +0100

    Fasta DB generation

--- analysis/csablast.py
+++ analysis/csablast.py
@@ -9,7 +9,27 @@ class FastaDbGen:
         self.evidenceLevel = evidenceLevel
 
     def generateFastaDb(self):
-        pass
+        self.fastaDb = {}
+
+        # Fetch genomes
+        self.cursor.execute('SELECT id, name, genomeSource FROM Genomes;')
+        genomes = self.cursor.fetchall()
+
+        # Fetch relevant arrays for genomes
+        for genome in genomes:
+            name = genome[1]
+            source = genome[2]
+            self.cursor.execute('SELECT id, spacers FROM CRISPR WHERE genomeId = ? AND evidenceLevel >= ?;',
+                                [genome[0], self.evidenceLevel])
+            arrays = self.cursor.fetchall()
+
+            # Fetch spacers for arrays
+            for array in arrays:
+                self.cursor.execute('SELECT sequence FROM Regions WHERE crisprId = ? AND type = ?;',
+                                    [array[0], 'Spacer'])
+                spacers = [x[0] for x in self.cursor.fetchall()]
+                for i in range(0, len(spacers)):
+                    self.fastaDb[array[0] + '-' + str(i)] = (spacers[i], name, source)
 
     def generateFasta(self):
         pass
