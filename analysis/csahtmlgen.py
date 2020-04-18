f82efbc9ed01873ef64e65b853b00c0936e2e884
Lucy Hancock <lucy@lucyhancock.tech>
2020-04-18 21:14:58 +0100

    Skeleton Jinja2 generation

--- analysis/csahtmlgen.py
+++ analysis/csahtmlgen.py
@@ -1,5 +1,6 @@
 from csastatcalc import StatCalc
 from csadistribution import DistCalc
+from jinja2 import Environment, select_autoescape
 
 class CsaHTMLGenerator:
     doGenerateBasic = False
@@ -22,12 +23,23 @@ class CsaHTMLGenerator:
         else:
             raise TypeError('Provided object is not of type DistCalc')
 
-    def generateHTML(self):
+    def generateHTML(self, templateLoader):
+        paramDict = self.assembleParamDict()
+        jinjaEnv = Environment(loader=templateLoader, autoescape=select_autoescape(['html', 'htm']))
+        template = jinjaEnv.get_template('csanalysis_output.html')
+        return template.render(paramDict)
+
+    def assembleParamDict(self):
         paramDict = {}
         paramDict['doGenerateBasic'] = self.doGenerateBasic
         paramDict['doGenerateDist'] = self.doGenerateDist
         if self.doGenerateBasic:
-            paramDict['statCalc'] = self.basicStatCalc
+            paramDict['crisprCounts'] = self.basicStatCalc.sourceCounts
+            paramDict['crisprPercs'] = self.basicStatCalc.sourcePercs
         if self.doGenerateDist:
-            paramDict['distCalc'] = self.distCalc
-        return ''
+            paramDict['lengthPng'] = self.distCalc.lengthDistributionB64
+            paramDict['spacerPng'] = self.distCalc.spacerDistributionB64
+            paramDict['spacerNzPng'] = self.distCalc.spacerDistributionNoZeroB64
+            paramDict['arrayPng'] = self.distCalc.arrayDistributionB64
+            paramDict['arraySpacerPng'] = self.distCalc.arraySpacerDistributionB64
+        return paramDict
