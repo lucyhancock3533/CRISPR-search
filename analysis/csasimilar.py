8a21c3f0bd95b2960b23dabc40777589a50a4536
Lucy Hancock <lucy@lucyhancock.tech>
2020-05-06 18:14:35 +0100

    Convert similarities for external DBs

--- analysis/csasimilar.py
+++ analysis/csasimilar.py
@@ -12,12 +12,14 @@ class SimilarityCalc:
     similarities = None
     blastDb = None
 
-    def __init__(self, cursor, evidenceLevel, similarityPercCutoff, dbPath, blastPath):
-        self.fastaDb = FastaDbGen(cursor, evidenceLevel)
-        self.cursor = cursor
-        self.evidenceLevel = evidenceLevel
-        fileName = '/' + str(uuid.uuid4()) + '.fa'
-        self.fastaDb.exportFastaFile(dbPath + fileName)
+    def __init__(self, cursor, evidenceLevel, similarityPercCutoff, dbPath, blastPath, generateDb=True):
+        fileName = ''
+        if generateDb:
+            self.fastaDb = FastaDbGen(cursor, evidenceLevel)
+            self.cursor = cursor
+            self.evidenceLevel = evidenceLevel
+            fileName = '/' + str(uuid.uuid4()) + '.fa'
+            self.fastaDb.exportFastaFile(dbPath + fileName)
         self.blastIns = BLAST(blastPath)
         self.blastIns.generateBlastDb(dbPath + fileName)
         self.similarityPercCutoff = similarityPercCutoff
@@ -51,9 +53,14 @@ class SimilarityCalc:
                             for i in range(1, len(blastRes)):
                                 if float(blastRes[i][1]) >= minBit:
                                     results.append(blastRes[i])
-            if len(results) > 0:
-                genomeResults.append((genome[1] + ' - ' + genome[2], [self.replaceIdForName(x) for x in results]))
+            if len(results) > 0 and self.fastaDb is not None:
+                genomeResults.append((genome[1] + ' - ' + genome[2], [self.replaceIdForNameFastaDb(x) for x in results]))
+            elif len(results) > 0 and self.fastaDb is None:
+                genomeResults.append((genome[1] + ' - ' + genome[2]))
         self.similarities = genomeResults
 
-    def replaceIdForName(self, ipt):
+    def replaceIdForNameFastaDb(self, ipt):
         return self.fastaDb.fastaDb[ipt[0]][1] + ' - ' + self.fastaDb.fastaDb[ipt[0]][2] + ' - ' + ipt[0], ipt[1], ipt[2], ipt[3]
+
+    def replaceId(self, ipt):
+        return ipt[0], ipt[1], ipt[2], ipt[3]
